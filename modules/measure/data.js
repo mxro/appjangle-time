cf5f60d0b6f9b04ad62749ee8895375af625f6c6
mxro <mxro@nowhere.com>
2014-10-13 10:35:41 +1100

    Defining first version for module data.

--- modules/measure/data.js
+++ modules/measure/data.js
@@ -1,2 +1,9 @@
-// <!-- one.download http://slicnet.com/mxrogm/mxrogm/apps/stream/r/2014/10/13/sr3/n/r/2014/10/13/sr/n/Unnamed15/node_se4 -->node.setValue("Let's rock!");
-// <!-- one.end -->
+// <!-- one.download http://slicnet.com/mxrogm/mxrogm/apps/stream/r/2014/10/13/sr3/n/r/2014/10/13/sr/n/Unnamed15/node_se4 -->node.append(t_measurement);
+
+node.append(new Date()).append(t_starttime);
+
+node.append(0).append(t_minutespassed);
+
+node.append("").append(t_activity);
+
+node.append("").append(t_endtime);// <!-- one.end -->
