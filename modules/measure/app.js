c637946057be50e1637305e3013c8f64080bf6e1
mxro <mxro@nowhere.com>
2014-10-13 09:35:22 +1100

    Create measure module.

---
+++ modules/measure/app.js
@@ -0,0 +1,5 @@
+// <!-- one.download http://slicnet.com/mxrogm/mxrogm/apps/stream/r/2014/10/13/sr3/n/r/2014/10/13/sr/n/node_ge7 -->node.get(function(node) {
+    $('.txt', elem).text(node.value());
+    
+});
+// <!-- one.end -->
---
+++ modules/measure/data.js
@@ -0,0 +1,2 @@
+// <!-- one.download http://slicnet.com/mxrogm/mxrogm/apps/stream/r/2014/10/13/sr3/n/r/2014/10/13/sr/n/Unnamed15/node_se4 -->node.setValue("Let's rock!");
+// <!-- one.end -->
